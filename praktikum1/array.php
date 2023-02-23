feb265dd002c88dc01fa06cf4372599a088a5464
ahmdnuraziz <ahmdnuraziz@gmail.com>
2023-02-23 12:31:12 +0700

    praktikum1

---
+++ praktikum1/array.php
@@ -0,0 +1,34 @@
+<?php
+// buat array
+$animals = ["burung", "kucing", "nyamuk", "singa"];
+echo $animals[1] . "<br>";
+
+echo $animals[3];
+
+foreach ($animals as $animal) {
+    echo $animal . "<br>";
+}
+
+// buat array assosiative
+$mahasiswa = ["nama"=>"Aziz", "jurusan" => "TI", "semester"=>2];
+echo $mahasiswa["jurusan"] . "<br>";
+
+// nampilin semua data
+foreach($mahasiswa as $key=>$value){
+    echo "key : " . $key . " value : " . $value . "<br>";
+}
+
+// array multi dimensi
+
+$dosen = [
+    ["pak rojul", "web"],
+    ["pak reza", "DDP"],
+    ["pak luqman", "OS"]
+];
+
+echo $dosen[0][0];
+
+//  00 01
+
+
+?>
---
+++ praktikum1/variabel.php
@@ -0,0 +1,11 @@
+<?php
+echo 'Hello word'."<br>";
+//buat variable
+$nama = "Aziz";
+$jurusan = "TI";
+$semester = 2;
+
+echo "nama saya ". $nama."<br>";
+echo "saya jurusan ". $jurusan ." semester ".$semester;
+
+?>
